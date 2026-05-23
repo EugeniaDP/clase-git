bbe5e809db99b805b2941680f6cc39c3b7673653
EugeniaDP <edepalma95@gmail.com>
2026-05-23 11:27:40 -0300

    Borrè

--- ej1.py
+++
@@ -1,5 +0,0 @@
-# Completa el programa para que imprima el nombre y la edad del usuario.
-
-nombre = input("Ingrese su nombre: ")
-edad = int(input("Ingrese su edad en anos: "))
-print('Te llamas:', nombre, 'y tenes', edad, 'anos.')
--- ej2.py
+++
@@ -1,7 +0,0 @@
-# Corregí el programa para que imprima solamente los múltiplos de 3.
-
-numero = int(input("Ingrese un número entero: "))
-
-for i in range(numero):
-    if i % 3 == 0:
-        print(i)
