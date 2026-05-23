c08d58a3a7909d720df8eeb5019b1ae7a1be3df5
EugeniaDP <edepalma95@gmail.com>
2026-05-23 10:38:39 -0300

    Agrego ejercicios

---
+++ ej1.py
@@ -0,0 +1,4 @@
+# Completá el programa para que imprima el nombre y la edad del usuario.
+
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad en años: "))
---
+++ ej2.py
@@ -0,0 +1,7 @@
+# Corregí el programa para que imprima solamente los múltiplos de 3.
+
+numero = int(input("Ingrese un número entero: "))
+
+for i in range(numero):
+    if i % 2 == 0:
+        print(i)
